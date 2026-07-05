43538c42abbf6593dca4f28c182b4fbf50c95dd8
Saeiddark <97860304+Saeiddark@users.noreply.github.com>
2026-07-06 01:15:32 +0330

    Add new commands to the command registry

--- commands/index.js
+++ commands/index.js
@@ -3,6 +3,11 @@ import helpCommand from './help.js';
 import newCommand from './new.js';
 import aboutCommand from './about.js';
 import settingsCommand from './settings.js';
+import tableCommand from './table.js';
+import nextteamCommand from './nextteam.js';
+import resultsCommand from './results.js';
+import scorersCommand from './scorers.js';
+import predictCommand from './predict.js';
 
 export function registerCommands(bot) {
   bot.command('start', startCommand);
@@ -10,4 +15,9 @@ export function registerCommands(bot) {
   bot.command('new', newCommand);
   bot.command('about', aboutCommand);
   bot.command('settings', settingsCommand);
+  bot.command('table', tableCommand);
+  bot.command('nextteam', nextteamCommand);
+  bot.command('results', resultsCommand);
+  bot.command('scorers', scorersCommand);
+  bot.command('predict', predictCommand);
 }
